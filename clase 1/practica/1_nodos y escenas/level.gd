8cded3242794bc1354b548e191506fe4e69c6983
Hernan <pradoh97@gmail.com>
2025-08-03 16:44:30 -0300

    Add coin collection code

--- clase 1/practica/1_nodos y escenas/level.gd
+++ clase 1/practica/1_nodos y escenas/level.gd
@@ -2,3 +2,8 @@ extends Node2D
 
 func _ready():
 	$Camera2D.make_current()
+	for coin in $Coins.get_children():
+		coin.connect("collected", add_coin)
+
+func add_coin():
+	%Score.text = str(int(%Score.text) + 1)
