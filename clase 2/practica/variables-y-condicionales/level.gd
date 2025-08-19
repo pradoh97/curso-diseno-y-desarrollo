68bc20dc091fbe7ee0252815fcbfd4d7fad84df5
Hernán <pradoh97@gmail.com>
2025-08-19 13:02:53 -0300

    Move ui update to spaceship

--- clase 2/practica/variables-y-condicionales/level.gd
+++ clase 2/practica/variables-y-condicionales/level.gd
@@ -7,16 +7,17 @@ var meteor_scene := preload("res://flying_object/meteor/meteor.tscn")
 var health_pack_scene := preload("res://flying_object/health_pack/health_pack.tscn")
 
 func _ready() -> void:
+	$Spaceship.health_bar = $UI/HealthBar
+	$Spaceship.killed.connect(func():
+		$MeteorTimer.stop()
+		$HealthPackTimer.stop()
+	)
 	for star in $Stars.get_children():
 		(star as CPUParticles2D).emitting = true
 
 func spawn_flying_object(flying_object_scene : PackedScene):
 	var flying_object : FlyingObject = flying_object_scene.instantiate()
 	add_child(flying_object)
-	if flying_object.deals_damage:
-		flying_object.collided.connect($UI/HealthBar.decrease)
-	else:
-		flying_object.collided.connect($UI/HealthBar.increase)
 	
 	flying_object.global_position = $SpawnPoints.get_children().pick_random().global_position
 	flying_object.collided.connect(flying_object.queue_free)
@@ -26,8 +27,7 @@ func _on_meteor_timer_timeout() -> void:
 	for amount in randi_range(1, max_meteors_to_spawn):
 		spawn_flying_object(meteor_scene)
 
-
 func _on_health_pack_timer_timeout() -> void:
-	$MeteorTimer.wait_time = randf_range(1.2, 1.5)
+	$HealthPackTimer.wait_time = randf_range(1.2, 1.5)
 	for amount in randi_range(1, max_health_packs_to_spawn):
 		spawn_flying_object(health_pack_scene)
--- clase 2/practica/variables-y-condicionales/spaceship/spaceship.gd
+++ clase 2/practica/variables-y-condicionales/spaceship/spaceship.gd
@@ -1,9 +1,14 @@
 extends CharacterBody2D
 
-const SPEED = 700.0
-const HEALTH = 100
+class_name Spaceship
 
-func _physics_process(delta: float) -> void:
+const SPEED = 800.0
+var health = 100.0
+var health_bar : HealthBar
+
+signal killed
+
+func _physics_process(_delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -11,3 +16,18 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func collide_flying_object(flying_object : FlyingObject):
+	if flying_object.deals_damage:
+		health = health - 20
+	else:
+		health = health + 20
+	health_bar.update(health)
+	if health <= 0:
+		kill_player()
+	
+func kill_player():
+	killed.emit()
+	set_physics_process(false)
+	$AnimationPlayer.play("die")
+	$AnimationPlayer.animation_finished.connect(get_tree().quit)
