2035ffa1ea2f76b69611e8a5d52e1cf1b5de88f3
Hernán <pradoh97@gmail.com>
2025-08-19 13:03:37 -0300

    Move collision management to spaceship

--- clase 2/practica/variables-y-condicionales/flying_object/flying_object.gd
+++ clase 2/practica/variables-y-condicionales/flying_object/flying_object.gd
@@ -1,16 +1,15 @@
 extends Area2D
 class_name FlyingObject
 
-signal collided
-
 @export var deals_damage : bool = true
 @export var max_speed = 1000
 @export var min_speed = 800
 var direction : Vector2
 var speed
 
-func _on_body_entered(_body: Node2D) -> void:
-	collided.emit()
+func _on_body_entered(body: Node2D) -> void:
+	(body as Spaceship).collide_flying_object(self)
+	queue_free()
 
 func _ready():
 	var horizontal_direction = [-1, 1].pick_random()
