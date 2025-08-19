b523844f4ddcc015e11c5efad443d02209a6bdde
Hernán <pradoh97@gmail.com>
2025-08-19 15:04:02 -0300

    Add health bar color change

--- clase 2/practica/variables-y-condicionales/health_bar/health_bar.gd
+++ clase 2/practica/variables-y-condicionales/health_bar/health_bar.gd
@@ -3,7 +3,8 @@ extends PanelContainer
 class_name HealthBar
 
 var max_health
-var health_factor = 5
+var warning_health = 60
+var critical_health = 40
 
 func _ready():
 	max_health = size.x
@@ -11,4 +12,13 @@ func _ready():
 
 func update(new_health : float):
 	var current_health_percent = new_health/100.0
-	$CurrentHealth.texture.width = clamp(max_health*current_health_percent, 1, INF)
+	
+	#Hacer cambios a partir de acá.
+	$CurrentHealth.texture.width = clamp(max_health*current_health_percent, 1, max_health)
+		
+	if new_health < critical_health:
+		$CurrentHealth.texture.gradient.colors[0] = Color("#f60028")
+	elif new_health < warning_health:
+		$CurrentHealth.texture.gradient.colors[0] = Color("#ab7a19")
+	else:
+		$CurrentHealth.texture.gradient.colors[0] = Color("#609543")
