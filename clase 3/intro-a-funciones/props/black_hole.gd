f9ab3ee4d1d3dab79a8695aa26ed4b8dbbb514b3
pradoh97 <pradoh97@gmail.com>
2025-09-06 12:32:17 -0300

    Add disabling black_hole and renamed script

--- clase 3/intro-a-funciones/props/black_hole.gd
+++ clase 3/intro-a-funciones/props/black_hole.gd
@@ -1,5 +1,11 @@
 extends Area2D
 
+@export var enabled = true
+
+func _ready():
+	if not enabled:
+		$CollisionShape2D.disabled = true
+
 func _on_body_entered(body):
 	(body as Ship).destruida = true
 	(body as Ship).velocity = Vector2.ZERO
