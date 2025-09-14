b297d30e35ad870b2cb9a01f0da61e04e447e7ed
pradoh97 <pradoh97@gmail.com>
2025-09-14 11:24:20 -0300

    Force animation start

--- clase 4/intro-a-ataques/dummy/dummy.gd
+++ clase 4/intro-a-ataques/dummy/dummy.gd
@@ -1,16 +1,6 @@
 extends Area2D
 
-var vulnerable : bool = true
-
 func _on_body_entered(_body):
-	if vulnerable:
-		print("ouch!")
-		vulnerable = false
-		$CollisionShape2D.set_deferred("disabled", true)
-		$AnimationPlayer.play("hit")
-
-
-func _on_animation_player_animation_finished(anim_name):
-	if anim_name == "hit":
-		vulnerable = true
-		$CollisionShape2D.set_deferred("disabled", false)
+	$AnimationPlayer.stop()
+	print("ouch!")
+	$AnimationPlayer.play("hit")
