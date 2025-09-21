e6f98912e969d2da4b8505325006f44ea26c3d92
pradoh97 <pradoh97@gmail.com>
2025-09-21 13:53:38 -0300

    Remove unused line

--- clase 5/1_cambio-de-escenas/level.gd
+++ clase 5/1_cambio-de-escenas/level.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 func _ready():
-	$Camera2D.make_current()
 	for coin in $Coins.get_children():
 		coin.connect("body_entered", add_coin)
 		coin.connect("body_entered", func(_body): coin.queue_free())
