3eabbbb756ce5ae09696bd90bfcc489a6bd00f38
pradoh97 <pradoh97@gmail.com>
2025-09-21 11:04:19 -0300

    Fix scene path

--- clase 3/bucles/lista_ubicaciones_base/lista_ubicaciones_base.gd
+++ clase 3/bucles/lista_ubicaciones_base/lista_ubicaciones_base.gd
@@ -2,7 +2,7 @@ extends VBoxContainer
 class_name ListaUbicacionesBase
 
 func agregar_ubicacion(nombre : String = ""):
-	var escena_ubicacion : PackedScene = load("res://ubicacion.tscn")
+	var escena_ubicacion : PackedScene = load("res://ubicacion/ubicacion.tscn")
 	var instancia : Ubicacion = escena_ubicacion.instantiate()
 	instancia.asignar_nombre(nombre)
 	add_child(instancia)
